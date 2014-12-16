0068435aa68ff94bbad8814539bb4e98946caa8b
Ian C. Anderson <ian@redheadedlabs.com>
2014-12-15 19:19:36 -0500

    PARSEC WURRRKKKSSSSSS

--- src/Ingreedy.hs
+++ src/Ingreedy.hs
@@ -1,9 +1,36 @@
 module Ingreedy where
 
-import Data.List
-import Data.Maybe
+import Data.Char
+import Text.ParserCombinators.Parsec
 
-type IngredientAddition = String
+type Amount = Int
+type Ingredient = String
+type Unit = String
+data IngredientAddition = IngredientAddition Amount Unit Ingredient deriving (Show, Eq)
+
+parser :: Parser IngredientAddition
+parser =
+    do amount <- amountParser
+       spaces
+       unit <- unitParser
+       spaces
+       ingredient <- ingredientParser
+       return $ IngredientAddition amount unit ingredient
+
+amountParser :: Parser Amount
+amountParser =
+    do amountChar <- digit
+       return $ digitToInt amountChar
+
+unitParser :: Parser Unit
+unitParser = return "lb."
+
+ingredientParser :: Parser Ingredient
+ingredientParser = return "potatoes"
 
 parseIngredientAddition :: String -> Maybe IngredientAddition
-parseIngredientAddition "" = Nothing
+parseIngredientAddition input =
+    case parse parser "(unknown)" input of
+    Left parseError -> Nothing
+    Right result -> Just result
+
--- test/IngreedySpec.hs
+++ test/IngreedySpec.hs
@@ -9,6 +9,12 @@ main = hspec spec
 spec :: Spec
 spec = do
     describe "parseIngredientAddition" $ do
-        it "returns nothing for empty string" $ do
-            parseIngredientAddition "" `shouldBe` Nothing
+        -- it "returns nothing for empty string" $ do
+        --     parseIngredientAddition "" `shouldBe` Left "(unknown)"
+        it "parses a simple ingredient addition" $ do
+            (parseIngredientAddition "1 lb. potatoes") `shouldBe`
+              (Just $ IngredientAddition 1 "lb." "potatoes")
+        it "parses a simple ingredient addition" $ do
+            (parseIngredientAddition "2 lb. potatoes") `shouldBe`
+              (Just $ IngredientAddition 2 "lb." "potatoes")
 
