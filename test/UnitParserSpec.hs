0963864a85945dc8240ea0fbf937a98fee1226e1
Ian C. Anderson <ian@redheadedlabs.com>
2015-01-16 16:18:40 -0500

    ewww 4 space indent

--- test/UnitParserSpec.hs
+++ test/UnitParserSpec.hs
@@ -17,14 +17,14 @@ parseUnit input =
 spec :: Spec
 spec = do
     describe "unitParser" $ do
-      it "parses an abbreviated unit" $ do
-          parseUnit "lb" `shouldBe` "lb"
-      it "parses an abbreviated unit with punctuation" $ do
-          parseUnit "lb." `shouldBe` "lb."
-      it "parses a full unit" $ do
-          parseUnit "pound" `shouldBe` "pound"
-      it "parses a plural unit" $ do
-          parseUnit "pounds" `shouldBe` "pounds"
-      it "doesn't parse non-units" $ do
-          parseUnit "table" `shouldBe` "failed"
+        it "parses an abbreviated unit" $ do
+            parseUnit "lb" `shouldBe` "lb"
+        it "parses an abbreviated unit with punctuation" $ do
+            parseUnit "lb." `shouldBe` "lb."
+        it "parses a full unit" $ do
+            parseUnit "pound" `shouldBe` "pound"
+        it "parses a plural unit" $ do
+            parseUnit "pounds" `shouldBe` "pounds"
+        it "doesn't parse non-units" $ do
+            parseUnit "table" `shouldBe` "failed"
 
